e377862eb83528f7213df986108430136093956b
beboofed <85536810+beboofed@users.noreply.github.com>
2025-04-02 05:42:43 -0500

    Update shutdown.ps1

--- src/shutdown.ps1
+++ src/shutdown.ps1
@@ -3,4 +3,4 @@
 $notify = new-object system.windows.forms.notifyicon
 $notify.icon = [System.Drawing.SystemIcons]::Information
 $notify.visible = $true
-$notify.showballoontip(10,"Success!","Terabyte Tweaker has finished tweaking your computer! It will now restart in 5 minutes!",[system.windows.forms.tooltipicon]::None)
+$notify.showballoontip(10,"Success!","Tweaker has finished tweaking your computer! It will now restart in 5 minutes!",[system.windows.forms.tooltipicon]::None)
